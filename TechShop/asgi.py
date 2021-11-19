9a199405d0c37274e86995e0ad32cbddf0d41c6f
kiri3914 <you@example.com>
2021-11-20 00:08:10 +0600

    ok

--- TechShop/asgi.py
+++ TechShop/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TechShop project.
+ASGI config for TechS project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechShop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechS.settings')
 
 application = get_asgi_application()
--- TechShop/wsgi.py
+++ TechShop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TechShop project.
+WSGI config for TechS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechShop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechS.settings')
 
 application = get_wsgi_application()
